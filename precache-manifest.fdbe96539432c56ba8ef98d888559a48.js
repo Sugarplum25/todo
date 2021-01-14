c6019f1d685fb713dd718f9c00c8ec7ee18ca301
Sugarplum25 <Sugarplamka@gmail.com>
2021-01-14 13:55:48 +0300

    Updates

---
+++ precache-manifest.fdbe96539432c56ba8ef98d888559a48.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "994683f1655fdc0931d436fc40c180fb",
+    "url": "/todo/index.html"
+  },
+  {
+    "revision": "ece53321d52b7a5de2c8",
+    "url": "/todo/static/css/main.8ce40f7d.chunk.css"
+  },
+  {
+    "revision": "d773ea7eeb956c0a38d5",
+    "url": "/todo/static/js/2.539091bc.chunk.js"
+  },
+  {
+    "revision": "c836aaadfe43dfd9fcb8ddeddfd61e18",
+    "url": "/todo/static/js/2.539091bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ece53321d52b7a5de2c8",
+    "url": "/todo/static/js/main.52dfdd99.chunk.js"
+  },
+  {
+    "revision": "42e23f9118876525bea8",
+    "url": "/todo/static/js/runtime-main.0d77e128.js"
+  }
+]);
